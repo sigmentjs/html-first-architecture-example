6aff4e1829c0de3f1e2c0ee414798720cb25d11b
sigmentjs <sigmentjs@gmail.com>
2025-08-25 19:04:54 +0300

    fix about

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,8 @@
 
 import 'sigment'
 
-import Routes from '../router/Routes.js';
-
 function About(props) {
-  debugger
-console.log(props)
 
-function handleNavigate(id) {
-  return (e) => {
-    NavigateTo(`about/${id}/2`, Routes, "about", true, e)
-      .then(() => console.log("Navigated to about"))
-     .catch(console.error);
-  };
-}
-  
   return div({ class: "about" },
     h2("About Component"),
     p(() => `ID: ${props.id ?? "no id"}`),
